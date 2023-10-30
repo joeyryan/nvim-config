8ee352144eb053671275574d8de50ff3f7216683
Joseph Ryan <jryan32@protonmail.com>
2023-10-30 09:39:56 -0400

    Updated nvim config

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -47,4 +47,37 @@ keymap("n", "<leader>c", "<cmd>bw<cr>", { desc = "Close buffer" })
 
 -- Bufferline
 keymap('n', '<tab>', "<cmd> BufferLineCycleNext <CR>", { desc = 'Go to next bufferline in order' })
-keymap('n', '<S-tab>', "<cmd> BufferLineCyclePrev <CR>", { desc = 'Go to previous bufferline in order' })
+keymap('n', '<S-tab>', "<cmd> BufferLineCyclePrev <CR>", { desc = 'Go to previous bufferline in [order' })
+
+keymap('n', '<C-Up>', "<cmd>resize -2<CR>", { desc = 'Resize split up' })
+keymap('n', '<C-Down>', "<cmd>resize +2<CR>", { desc = 'Resize split down' })
+keymap('n', '<C-Left>', "<cmd>vertical resize -2<CR>", { desc = 'Resize split left' })
+keymap('n', '<C-Right>', "<cmd>vertical resize +2<CR>", { desc = 'Resize split right' })
+
+
+
+
+------ needs work:
+
+-- keymap('n', "<leader>fb", function() require("telescope.builtin").buffers() end, { desc = "Find buffers" })
+-- keymap('n', "<leader>fc", function() require("telescope.builtin").grep_string() end, { desc = "Find word under cursor" })
+-- keymap('n', "<leader>fC", function() require("telescope.builtin").commands() end, { desc = "Find commands" })
+-- keymap('n', "<leader>ff", function() require("telescope.builtin").find_files() end, { desc = "Find files" })
+-- keymap('n', "<leader>fF",
+--   function() require("telescope.builtin").find_files { hidden = true, no_ignore = true } end,
+--   { desc = "Find all files" })
+
+
+-- keymap('n', "<leader>fh", function() require("telescope.builtin").help_tags() end, { desc = "Find help" })
+-- keymap('n', "<leader>fk", function() require("telescope.builtin").keymaps() end, { desc = "Find keymaps" })
+-- keymap('n', "<leader>fm", function() require("telescope.builtin").man_pages() end, { desc = "Find man" })
+
+-- keymap('n', "<leader>fo", function() require("telescope.builtin").oldfiles() end, { desc = "Find history" })
+-- keymap('n', "<leader>fr", function() require("telescope.builtin").registers() end, { desc = "Find registers" })
+-- keymap('n', "<leader>fw", function() require("telescope.builtin").live_grep() end, { desc = "Find words" })
+-- keymap('n', "<leader>fW",
+--   function()
+--     require("telescope.builtin").live_grep {
+--       additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+--     }
+--   end, { desc = "Find words in all files" })
