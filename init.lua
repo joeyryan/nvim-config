343a2d70a49acb024574bd0924a64e48abe063d6
Joseph Ryan <jryan32@protonmail.com>
2023-11-12 20:43:01 -0500

    Forgot to add necessary file to commit 7930c013

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 LAZY_PLUGIN_SPEC = {}
 
-function spec(item)
+local function spec(item)
   table.insert(LAZY_PLUGIN_SPEC, { import = item })
 end
 
@@ -15,23 +15,20 @@ else
   spec "plugin.bufferline"
   spec "plugin.devicons"
   spec "plugin.treesitter"
-  spec "plugin.mason"
   spec "plugin.schemastore"
-  spec "plugin.lspconfig"
-  spec "plugin.navic"
+  spec "plugin.lsp_config"
   spec "plugin.breadcrumbs"
   spec "plugin.null-ls"
   spec "plugin.illuminate"
   spec "plugin.telescope"
-  spec "plugin.telescope-tabs"
+  -- spec "plugin.telescope-tabs"
   spec "plugin.nvimtree"
   spec "plugin.lualine"
   spec "plugin.whichkey"
   spec "plugin.cmp"
-  spec "plugin.autopairs"
   spec "plugin.comment"
   spec "plugin.gitsigns"
-  spec "plugin.neogit"
+  -- spec "plugin.neogit"
   spec "plugin.diffview"
   spec "plugin.indentline"
   spec "plugin.alpha"
@@ -39,5 +36,6 @@ else
   spec "plugin.project"
   spec "plugin.toggleterm"
   spec "plugin.bufdelete"
+  -- spec "plugin.dapui"
   require "plugin.lazy"
 end
