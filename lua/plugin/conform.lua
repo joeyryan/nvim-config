0c6749c42537e6e4b86b4a934667b0a0cba8c22d
Joseph Ryan <jryan32@protonmail.com>
2023-11-14 23:38:27 -0500

    fixes

--- lua/plugin/conform.lua
+++ lua/plugin/conform.lua
@@ -1,33 +1,37 @@
 local M = {
-  'stevearc/conform.nvim',
-  opts = {},
+	"stevearc/conform.nvim",
+	opts = {},
 }
 
-
 function M.config()
-  require("conform").setup({
-    formatters_by_ft = {
-      --lua = { "stylua" },
-      -- Conform will run multiple formatters sequentially
-      --python = { "isort", "black" },
-      -- Use a sub-list to run only the first available formatter
-      html = { { "prettierd", "prettier" } },
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      ["*"] = { { "prettierd", "prettier" } },
-    },
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 5000,
-      lsp_fallback = true,
-    },
-  })
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-      require("conform").format({ bufnr = args.buf })
-    end,
-  })
+	require("conform").setup({
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			svelte = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			graphql = { "prettier" },
+			lua = { "stylua" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 2000,
+			lsp_fallback = true,
+			async = false,
+		},
+	})
+	--vim.api.nvim_create_autocmd("BufWritePre", {
+	--	pattern = "*",
+	--	callback = function(args)
+	--		require("conform").format({ bufnr = args.buf })
+	--	end,
+	--})
 end
 
 return M
