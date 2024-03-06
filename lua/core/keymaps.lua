a66e10ecec76ca606b9d74f2f4bf0182e7ebc609
Joseph Ryan <jryan32@protonmail.com>
2024-03-06 15:08:55 -0500

    minor changes to keymaps, added scrolloff

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -22,56 +22,29 @@ keymap("n", "#", "#zz", opts)
 keymap("n", "g*", "g*zz", opts)
 keymap("n", "g#", "g#zz", opts)
 
--- Stay in indent mode
+-- Stay in visual mode so you can keep indenting
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+-- Don't copy replaced text into register on paste
 keymap("x", "p", [["_dP]])
 
-vim.cmd [[:amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definition()<CR>]]
-vim.cmd [[:amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]]
+vim.cmd([[:amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definition()<CR>]])
+vim.cmd([[:amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]])
 -- vim.cmd [[:amenu 10.120 mousemenu.-sep- *]]
 
 keymap("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
 keymap("n", "<Tab>", "<cmd>:popup mousemenu<CR>")
 
 -- Swap between windows easier
-keymap('n', '<C-h>', "<C-w>h", { desc = 'Window left' })
-keymap('n', '<C-l>', "<C-w>l", { desc = 'Window right' })
-keymap('n', '<C-j>', "<C-w>j", { desc = 'Window down' })
-keymap('n', '<C-k>', "<C-w>k", { desc = 'Window up' })
+keymap("n", "<C-h>", "<C-w>h", { desc = "Window left" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Window right" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Window down" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Window up" })
 
-keymap('n', '<C-\\>', "<C-w>v", { desc = 'Split view vertically' })
+keymap("n", "<C-\\>", "<C-w>v", { desc = "Split view vertically" })
 
-keymap('n', '<C-Up>', "<cmd>resize -2<CR>", { desc = 'Resize split up' })
-keymap('n', '<C-Down>', "<cmd>resize +2<CR>", { desc = 'Resize split down' })
-keymap('n', '<C-Left>', "<cmd>vertical resize -2<CR>", { desc = 'Resize split left' })
-keymap('n', '<C-Right>', "<cmd>vertical resize +2<CR>", { desc = 'Resize split right' })
-
-
-
-
------- needs work:
-
--- keymap('n', "<leader>fb", function() require("telescope.builtin").buffers() end, { desc = "Find buffers" })
--- keymap('n', "<leader>fc", function() require("telescope.builtin").grep_string() end, { desc = "Find word under cursor" })
--- keymap('n', "<leader>fC", function() require("telescope.builtin").commands() end, { desc = "Find commands" })
--- keymap('n', "<leader>ff", function() require("telescope.builtin").find_files() end, { desc = "Find files" })
--- keymap('n', "<leader>fF",
---   function() require("telescope.builtin").find_files { hidden = true, no_ignore = true } end,
---   { desc = "Find all files" })
-
-
--- keymap('n', "<leader>fh", function() require("telescope.builtin").help_tags() end, { desc = "Find help" })
--- keymap('n', "<leader>fk", function() require("telescope.builtin").keymaps() end, { desc = "Find keymaps" })
--- keymap('n', "<leader>fm", function() require("telescope.builtin").man_pages() end, { desc = "Find man" })
-
--- keymap('n', "<leader>fo", function() require("telescope.builtin").oldfiles() end, { desc = "Find history" })
--- keymap('n', "<leader>fr", function() require("telescope.builtin").registers() end, { desc = "Find registers" })
--- keymap('n', "<leader>fw", function() require("telescope.builtin").live_grep() end, { desc = "Find words" })
--- keymap('n', "<leader>fW",
---   function()
---     require("telescope.builtin").live_grep {
---       additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
---     }
---   end, { desc = "Find words in all files" })
+keymap("n", "<C-Up>", "<cmd>resize -2<CR>", { desc = "Resize split up" })
+keymap("n", "<C-Down>", "<cmd>resize +2<CR>", { desc = "Resize split down" })
+keymap("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Resize split left" })
+keymap("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Resize split right" })
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -33,21 +33,21 @@ vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
 vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false -- display lines as one long line
-vim.opt.scrolloff = 0
+vim.opt.scrolloff = 1
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
 vim.opt.title = false
 -- colorcolumn = "80",
 -- colorcolumn = "120",
 vim.opt.fillchars = vim.opt.fillchars + "eob: "
-vim.opt.fillchars:append {
-  stl = " ",
-}
+vim.opt.fillchars:append({
+	stl = " ",
+})
 
-vim.opt.shortmess:append "c"
+vim.opt.shortmess:append("c")
 
-vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd([[set iskeyword+=-]])
 
 vim.g.netrw_banner = 0
 vim.g.netrw_mouse = 2
