4a6803a4155ee84bc4bf36ba89244dfbeaabb0df
Joseph Ryan <jryan32@protonmail.com>
2024-04-11 00:20:17 -0400

    icon change for gitsigns

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -23,13 +23,13 @@ function M.config()
       },
       delete = {
         hl = "GitSignsDelete",
-        text = icons.ui.TriangleArrowRight,
+        text = icons.ui.Triangle,
         numhl = "GitSignsDeleteNr",
         linehl = "GitSignsDeleteLn",
       },
       topdelete = {
         hl = "GitSignsDelete",
-        text = icons.ui.TriangleArrowRight,
+        text = icons.ui.Triangle,
         numhl = "GitSignsDeleteNr",
         linehl = "GitSignsDeleteLn",
       },
