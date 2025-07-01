51293f5efe60ca602ec7ad78e5fcfea5119364c8
Joseph Ryan <jryan32@protonmail.com>
2025-07-01 14:04:49 -0400

    Add cool new ui-select to telescope

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -4,6 +4,7 @@ local M = {
   dependencies = {
     -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true },
     { "nvim-telescope/telescope-fzf-native.nvim", lazy = true },
+    { "nvim-telescope/telescope-ui-select.nvim", lazy = true },
   },
   lazy = true,
   cmd = "Telescope",
@@ -150,10 +151,30 @@ function M.config()
         override_file_sorter = true, -- override the file sorter
         case_mode = "smart_case", -- or "ignore_case" or "respect_case"
       },
+      ["ui-select"] = {
+        require("telescope.themes").get_dropdown({
+          initial_mode = "normal",
+        }),
+
+        -- pseudo code / specification for writing custom displays, like the one
+        -- for "codeactions"
+        -- specific_opts = {
+        --   [kind] = {
+        --     make_indexed = function(items) -> indexed_items, width,
+        --     make_displayer = function(widths) -> displayer
+        --     make_display = function(displayer) -> function(e)
+        --     make_ordinal = function(e) -> string
+        --   },
+        --   -- for example to disable the custom builtin "codeactions" display
+        --      do the following
+        --   codeactions = false,
+        -- }
+      },
     },
   })
 
   telescope.load_extension("fzf")
+  telescope.load_extension("ui-select")
 
   local keymap = vim.api.nvim_set_keymap
   keymap("n", "<leader><leader>", "<cmd> Telescope buffers<CR>", { desc = "Open list of buffers" })
