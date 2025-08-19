e99910ded377187d3d658e7ab5d71420798dd494
Joseph Ryan <jryan32@protonmail.com>
2025-08-19 12:53:58 -0400

    updated clang LSP config

--- lua/plugin/lspsettings/clangd.lua
+++ lua/plugin/lspsettings/clangd.lua
@@ -1,5 +1,6 @@
 return {
-  cmd = { "clangd" }, -- optional, if it's not in PATH or you want a specific binary
+  -- cmd = { "clangd" }, -- optional, if it's not in PATH or you want a specific binary
+  cmd = { "clangd", "--compile-commands-dir=build", "--background-index", "--all-scopes-completion", "--clang-tidy" },
   filetypes = { "c", "cpp", "objc", "objcpp" },
   root_dir = require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
   init_options = {
