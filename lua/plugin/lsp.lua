eac87f41371d2590900c60ed6499f3e8b9e0cfba
Joseph Ryan <jryan32@protonmail.com>
2025-12-14 21:50:07 -0500

    updated LSP config

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -1,43 +1,17 @@
--- A few plugins to enable and configure LSP support
-local M = {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    {
-      "folke/neodev.nvim",
-    },
-    {
-      "folke/lazydev.nvim",
-    },
-    {
-      "nvim-lua/plenary.nvim",
-    },
-    {
-      "b0o/schemastore.nvim",
-    },
-    {
-      "ray-x/lsp_signature.nvim",
-    },
-  },
-  opts = {
-    inlay_hints = { enabled = true },
-  },
-}
-
-M.servers = {
+-- LSP configuration and related plugins
+local servers = {
   "kotlin_lsp",
   "phpactor",
   "clangd",
   "lua_ls",
   "cssls",
   "html",
-  -- "tsserver",
   "pyright",
   "bashls",
   "jsonls",
   "yamlls",
   "marksman",
   "tailwindcss",
-  -- "csharp_ls",
   "eslint",
   "angularls",
   "omnisharp",
@@ -47,24 +21,21 @@ M.servers = {
   "cmake",
 }
 
-function M.on_attach(client, bufnr)
+local function on_attach(client, bufnr)
   local opts = { noremap = true, silent = true }
   local keymap = vim.api.nvim_buf_set_keymap
   keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
   keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  -- keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
   keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover({ max_width = 100, wrap = true, border = 'single' })<CR>", opts)
   keymap(bufnr, "i", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
   keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  -- keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  -- keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 
   -- Modified gi and gr to use Trouble, which is a nicer list view than quickfix
   keymap(bufnr, "n", "gi", "<cmd>lua require('trouble').toggle('lsp_implementations')<cr>", opts)
   keymap(bufnr, "n", "gr", "<cmd>lua require('trouble').toggle('lsp_references')<cr>", opts)
 end
 
-function M.common_capabilities()
+local function common_capabilities()
   local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
   if status_ok then
     return cmp_nvim_lsp.default_capabilities()
@@ -82,7 +53,7 @@ function M.common_capabilities()
   return capabilities
 end
 
-function M.config()
+local function config()
   local icons = require("core.icons")
 
   local default_diagnostic_config = {
@@ -111,11 +82,11 @@ function M.config()
 
   vim.diagnostic.config(default_diagnostic_config)
 
-  -- Setup LSP for each server in the servers list using new vim.lsp.config API
-  for _, server in pairs(M.servers) do
+  -- Setup LSP for each server
+  for _, server in pairs(servers) do
     local opts = {
-      on_attach = M.on_attach,
-      capabilities = M.common_capabilities(),
+      on_attach = on_attach,
+      capabilities = common_capabilities(),
     }
 
     -- Check lspsettings folder for plugin specific config and load if found
@@ -124,22 +95,55 @@ function M.config()
       opts = vim.tbl_deep_extend("force", settings, opts)
     end
 
+    -- 2026-03-07 - NEODEV NO LONGER NEEDED WHEN USING LAZYDEV.NVIM
     -- neodev is a lua specific plugin that loads neovim documentation into the lsp
-    if server == "lua_ls" then
-      require("neodev").setup({})
-    end
-
-    -- Use the new vim.lsp.config API
+    -- if server == "lua_ls" then
+    --   require("neodev").setup({})
+    -- end
+    --
+    -- Use the new vim.lsp.config instead of nvim-lspconfig
     vim.lsp.config[server] = opts
     vim.lsp.enable(server)
   end
-
-  require("lsp_signature").setup({
-    zindex = 20,
-    handler_opts = {
-      border = "single",
-    },
-  })
 end
 
-return M
+-- Return an array of plugin specs instead of a single spec
+return {
+  -- Core LSP plugin (only needed for Neovim < 0.11)
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = config,
+  },
+
+  -- Neodev for Lua development
+  {
+    "folke/neodev.nvim",
+    ft = "lua",
+  },
+
+  -- LazyDev for lazy-loading Neovim Lua API
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {},
+  },
+
+  -- Schemastore for JSON schemas
+  {
+    "b0o/schemastore.nvim",
+    ft = "json",
+  },
+
+  -- LSP signature help
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+    opts = {
+      zindex = 20,
+      handler_opts = {
+        border = "single",
+      },
+    },
+  },
+}
